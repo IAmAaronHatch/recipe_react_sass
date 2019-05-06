43ccc10add9160d07b62ee8e4b30fdb4c096ebd7
Aaron Hatch <aaron@Aarons-MBP.attlocal.net>
2019-05-06 13:54:43 -0700

    added env file to hold key and id for api

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+require('dotenv').config();
+
+const App = () => {
+
+  const APP_ID = process.env.REACT_APP_EDAMAM_API_ID;
+
+  const APP_KEY = process.env.REACT_APP_EDAMAM_API_KEY
+
   return (
-    <div className="App">
+    <div className='App'>
       <h1> Recipe App </h1>
     </div>
   );
