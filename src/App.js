100258dc7882458c0354b5bc088a49ab08a43d91
Aaron Hatch <aaron@Aarons-MBP.attlocal.net>
2019-05-06 14:07:17 -0700

    using useEffect, created a counter to see the useEffect func

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 require('dotenv').config();
@@ -8,11 +7,24 @@ const App = () => {
 
   const APP_ID = process.env.REACT_APP_EDAMAM_API_ID;
 
-  const APP_KEY = process.env.REACT_APP_EDAMAM_API_KEY
+  const APP_KEY = process.env.REACT_APP_EDAMAM_API_KEY;
+
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  useEffect(() => {
+    console.log('effect running')
+  }, []);
+
+  const [counter, setCounter] = useState(0);
 
   return (
     <div className='App'>
-      <h1> Recipe App </h1>
+      <form className='search-form'>
+        <input className='search-bar' type="text" />
+        <button className='search-btn' type='submit'> Search </button>
+      </form>
+
+      <h1 onClick={() => setCounter(counter + 1)} > { counter } </h1>
     </div>
   );
 }
