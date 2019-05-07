8d360fd22533e484d19ff51ebf5d7a7bf3358c80
Aaron Hatch <aaron@Aarons-MacBook-Pro.local>
2019-05-07 13:09:38 -0700

    updating the search bar works, selecting different recipes with a query

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Recipe from './components/recipe'
 import './App.css';
 
@@ -10,7 +10,9 @@ const App = () => {
 
   const APP_KEY = '9c72b9bb0f4a96af402080b935881d14';
 
-  const [ recipes, setRecipes ] = useState([])
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('chicken')
 
 
   // commenting out because of limits to API call
@@ -18,25 +20,43 @@ const App = () => {
 
   // useEffect(() => {
   //   getRecipes();
-  // }, []);
+  // }, [query]);
 
   // const getRecipes = async () => {
-  //   const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+  //   const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
   //   const data = await response.json();
   //   console.log("data", data)
   //   console.log("data.hits", data.hits)
   //   setRecipes(data.hits)
   // }
 
+
+  const updateSearch = event => {
+    setSearch(event.target.value);
+  }
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+  }
   return (
     <div className='App'>
-      <form className='search-form'>
-        <input className='search-bar' type="text" />
+      <form 
+      className='search-form'
+      onSubmit={getSearch}
+      >
+        <input
+          className='search-bar'
+          type="text"
+          value={search}
+          onChange={updateSearch} />
         <button className='search-btn' type='submit'> Search </button>
       </form>
 
-      { recipes.map(recipe => (
-        <Recipe 
+      {recipes.map(recipe => (
+        <Recipe
+          // Change key later, set it equal to something a little more unique than just the title
+          key={recipe.recipe.label}
           title={recipe.recipe.label}
           image={recipe.recipe.image}
           calories={recipe.recipe.calories}
